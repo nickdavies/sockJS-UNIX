2ca9e114e9f29382d6c940a813a51fb0b3c28a70
Nick Davies <nick+git@freelancer.com>
2013-07-16 11:00:58 +1000

    removed console statements and added tracking to connection counts

--- server.js
+++ server.js
@@ -14,10 +14,14 @@ var server = http.createServer();
 // SockJS server
 var sockjs_tcp = sockjs.createServer(settings.sockjs_server);
 
+var socket_count = 0;
+var backend_count = 0;
+
 sockjs_tcp.on('connection', function(socket) {
+    socket_count ++;
     var reply_buffer = [];
 
-    var backend = net.createConnection({path: settings.backend_socket})
+    var backend = net.createConnection({path: settings.backend_socket}, function(){backend_count++;})
     backend.setKeepAlive(true);
     backend.write(JSON.stringify({"id": socket.id}));
 
@@ -35,35 +39,48 @@ sockjs_tcp.on('connection', function(socket) {
             console.log("bad packet:", message, err);
             return;
         }
-        console.log("proxied: ", message)
         backend.write(message);
     });
 
     backend.on('data', function(packet){
-        console.log('proxy reply:', packet.toString());
 
         reply_buffer += packet.toString();
         if (reply_buffer.indexOf('\n') != -1) {
 
             var lines = reply_buffer.split('\n');
             for (var i = 0; i < lines.length - 1; i++){
-                console.log('sent:', lines[i]);
                 socket.write(lines[i]);
             }
             reply_buffer = lines[lines.length - 1];
         }
-        console.log("buffer:", reply_buffer);
     });
+
+    var socket_closed = false;
+    var backend_closed = false;
 
     // On End
     socket.on('close', function(){
+        if (socket_closed) {
+            return;
+        }
+        socket_closed = true;
+        socket_count--;
+
         console.log("socket closed killing backend", arguments);
         backend.end();
     });
+
     backend.on('close', function(){
+        if (backend_closed) {
+            return;
+        }
+        backend_closed = true;
+        backend_count--;
+
         console.log("backend closed killing socket");
         socket.end();
     });
+
     backend.on('error', function(){
         console.log("an error!", arguments);
         socket.end();
@@ -76,3 +93,6 @@ sockjs_tcp.installHandlers(server);
 console.log(' [*] Listening on 0.0.0.0:8080' );
 server.listen(settings.listen_port, '0.0.0.0');
 
+setInterval(function(){
+    console.log("Connections:", socket_count, backend_count)
+}, 2000);
